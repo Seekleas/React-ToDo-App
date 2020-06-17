51fc5b79ebc43acff30f9e64ce0ed1a81948c0e9
Marcin Porada <marcin_porada@interia.pl>
2020-06-17 14:33:53 +0200

    Updates

---
+++ precache-manifest.515a0bafb0481f16d9d97aa2d0cceaca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11307083b00463ae967feabcf7fb73fc",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "06484b32f7ba83488d7f",
+    "url": "/React-ToDo-App/static/css/main.00338337.chunk.css"
+  },
+  {
+    "revision": "aaac275e29fdba8a12ca",
+    "url": "/React-ToDo-App/static/js/2.85acb023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-App/static/js/2.85acb023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06484b32f7ba83488d7f",
+    "url": "/React-ToDo-App/static/js/main.7bae5df6.chunk.js"
+  },
+  {
+    "revision": "e1860dc7a83551a2cf5c",
+    "url": "/React-ToDo-App/static/js/runtime-main.c6ab7763.js"
+  }
+]);
